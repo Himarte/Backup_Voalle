08ab2d64b8af15adb2dba7541b28b4b67160d9d0
JacobsenNando <fernandofjlr@gmail.com>
2024-01-30 10:04:42 -0300

    Up segurança

---
+++ credentials.py
@@ -0,0 +1,13 @@
+host1 = "synsuite.himarte.com.br"
+username1 = "backup_erp"
+password1 = "4WCDfvq!"
+port1 = 22
+host2 = "190.111.179.188"
+username2 = "backup_erp"
+password2 = "4WCDfvq!"
+port2 = 22512
+local_file_path1 = "/mnt/MestreDosMagos/Sistemas/bkpVoalle/Dados"
+remote_file_path1 = "/bkp/"
+local_file_path2 = "/mnt/MestreDosMagos/Sistemas/bkpVoalle/Radius"
+remote_file_path2 = "/bkp/"
+
